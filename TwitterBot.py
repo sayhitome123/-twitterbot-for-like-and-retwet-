665fd94eddd032907d18ef066f702aa354272eb8
sayhitome123 <55015679+sayhitome123@users.noreply.github.com>
2020-06-05 01:18:52 +0100

    Add files via upload

--- TwitterBot.py
+++ TwitterBot.py
@@ -16,37 +16,56 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 print('Account name: ' + user.name)
-def get_tweets(api,username):
-    page=1
-    deadend=False
-    while True:
-        tweets=api.list_timeline(list_id='1265742486405812225',include_rts=False) #Doing Libya politics list
+print('------------------------')
+time.sleep(2)
+Looop=2
+Now=datetime.datetime.now()
+while Looop <=5:
+
+    tweets=api.list_timeline(list_id='1265742486405812225',include_rts=False) #Doing Libya politics list
+
+#def get_tweets(api, username):
+    #while True:
         #if you want to do it for your home timeline: api.home_timeline()
         #if you want it for user do it for user api.user_timeline(username,page=page)
-        Now=datetime.datetime.now()
+    for tweet in tweets:
+        if (Now - tweet.created_at).days < 2:
+            print('User Name: ' + tweet.user.screen_name)
+            print('Date: ')
+            print(tweet.created_at)
+            try:
+                time.sleep(2)
+                tweet.favorite()
+                print('Tweet Liked - ' + tweet.text)
+                time.sleep(1)
+                print('Waiting for 07 Seconds')
+                time.sleep(7)
 
-        for tweet in tweets:
-            if (Now - tweet.created_at).days < 2:
-                print('------------------------')
-                print('User Name: ' + tweet.user.screen_name)
-                print('Date: ')
-                print(tweet.created_at)
-                try:
-                    tweet.favorite()
-                    print('Tweet Liked - ' + tweet.text)
-                    time.sleep(2)
-                    if tweet.retweet_count > 5:
-                        tweet.retweet()
-                        print('Retweeted')
-                        time.sleep(120)
-                except tweepy.TweepError as e:
-                    print(e.reason)
-                    time.sleep(90)
-                except StopIteration:
-                    break
-            else:
-                deadend= True
-                return
-            if not deadend:
-                page=1
-                time.sleep(20)
+
+            except tweepy.TweepError as e:
+                print(e.reason)
+                print('Waiting for 08 Seconds')
+                time.sleep(8)
+
+
+        if tweet.retweet_count > 5:
+            try:
+                tweet.retweet()
+                print('Retweeted')
+                print('Waiting for 09 Seconds')
+                time.sleep(9)
+                break
+
+            except tweepy.TweepError as e:
+                print(e.reason)
+                print('Waiting for 10 Seconds')
+                time.sleep(10)
+                break
+        else:
+            print('Retweets are less than 5')
+            time.sleep(2)
+            break
+
+    print('Waiting for 50 Seconds')
+    print('------------------------')
+    time.sleep(60)
