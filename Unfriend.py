079a5d8238eadbec44bd971916d81c4666f5bae5
sayhitome123 <55015679+sayhitome123@users.noreply.github.com>
2020-06-11 02:40:53 +0100

    Add files via upload

---
+++ Unfriend.py
@@ -0,0 +1,28 @@
+import sys
+import tweepy
+import datetime,time
+import os
+from os import environ
+
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+muted_users = api.mutes()
+friends_ids = api.friends_ids(api.me().id)
+
+for user in muted_users:
+    if user not in friends_ids:
+        print(user.screen_name+ ' unfriend')
+        api.destroy_mute(user.id)
+        api.destroy_friendship(user.id)
+        time.sleep(2)
+    else:
+        print('break')
+        break
